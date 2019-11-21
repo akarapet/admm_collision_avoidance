634e0c9210a1ca803465bb3d73a8bdf99680bfca
Aren Karapetyan <karapetyanaren@gmail.com>
2019-11-21 10:45:20 +0000

    finalised version, not working, visualise added

---
+++ admm_visualise.m
@@ -0,0 +1,64 @@
+function admm_visualise (r,x,N,T)
+
+x_1_plot = [];
+y_1_plot = [];
+
+x_2_plot =[];
+y_2_plot =[];
+
+x_3_plot =[];
+y_3_plot =[];
+
+
+    
+figure;
+
+
+pause on
+
+ 
+for k = 1:(N+1)
+    
+    % reference 
+plot(r{1}(1),r{1}(2),'kx')
+ 
+hold on
+
+% initial point
+%plot(x_1_0(1),x_1_0(2),'ro',x_2_0(1),x_2_0(2),'ro',x_3_0(1),x_3_0(2),'ro')
+plot(value(x{1,1}(1)),value(x{1,1}(2)),'ro',value(x{2,1}(1)),value(x{2,1}(2)),'ro',value(x{3,1}(1)),value(x{3,1}(2)),'ro')
+
+hold on
+    
+    
+    x_1_plot = [x_1_plot, value(x{1,k}(1))];
+    y_1_plot = [y_1_plot, value(x{1,k}(2))];
+    
+    x_2_plot = [x_2_plot, value(x{2,k}(1))];
+    y_2_plot = [y_2_plot, value(x{2,k}(2))];
+
+    x_3_plot = [x_3_plot, value(x{3,k}(1))];
+    y_3_plot = [y_3_plot, value(x{3,k}(2))];
+    
+
+
+    p = plot(x_1_plot,y_1_plot,'g',x_2_plot,y_2_plot,'b',x_3_plot,y_3_plot,'r');
+    hold on
+    
+    %legend('Agent 1','Agent 2','Agent3')
+    
+    
+    plot(value(x{1,k}(1)),value(x{1,k}(2)),'g*',value(x{2,k}(1)),value(x{2,k}(2)),'b*',value(x{3,k}(1)),value(x{3,k}(2)),'r*')
+    hold off
+    
+    legend('off')
+    %hold on
+    
+    %plot(value(x{1,k}(1)),value(x{1,k}(2)),'b*',value(x{2,k}(1)),value(x{2,k}(2)),'b*',value(x{3,k}(1)),value(x{3,k}(2)),'b*')
+
+    pause(T)
+end
+
+legend(p,'Agent 1','Agent 2','Agent3')
+
+end
--- decentralised_admm.m
+++ decentralised_admm.m
@@ -37,10 +37,10 @@ for i = 1:M
     
     % position reference setpoint
     r{i} = sdpvar(nu*N,1);
-    r{i} = ones(nu*N,1)*0;
+    r{i} = ones(nu*N,1)*1;
     
     % nominal position 
-    x_nominal(i,:) = sdpvar(repmat(nx,1,N+1),repmat(1,1,N+1));
+    %w_nominal(i,:) = sdpvar(repmat(nx,1,N+1),repmat(1,1,N+1));
     
 
     % w and w_to_j
@@ -54,26 +54,26 @@ end
     w_from_j = zeros(M*(M)*nu,N+1);
 
     % lambda, lambda_to_j and lambda_from_j
-    lambda = ones(M*nu,1) * 0.1;
-    lambda_to_j = ones(M*nu,M) * 0.1;
-    lambda_from_j = ones(M*nu,M) * 0.1;
+    lambda = ones(M*nu,1) * 0.01;
+    lambda_to_j = ones(M*nu,M) * 0.01;
+    lambda_from_j = ones(M*nu,M) * 0.01;
     
 
 
 for k = 1:N
     
     %  populate nominal values
-    w_nominal{1,k} = ones(nx,1)*0.05;
-    w_nominal{2,k} = -0.01 * ones(nx,1);
+    w_nominal{1,k} = ones(nx,1)*0.65;
+    w_nominal{2,k} = -0.21 * ones(nx,1);
     w_nominal{3,k} = 0 * ones(nx,1);
     
-    w_nominal{4,k} = ones(nx,1)*0.05;
-    w_nominal{5,k} = -0.01 * ones(nx,1);
-    w_nominal{6,k} = 0 * ones(nx,1);
+    w_nominal{4,k} = ones(nu,1)*0.25;
+    w_nominal{5,k} = -0.11 * ones(nu,1);
+    w_nominal{6,k} = 0.4 * ones(nu,1);
     
-    w_nominal{7,k} = ones(nx,1)*0.05;
-    w_nominal{8,k} = -0.01 * ones(nx,1);
-    w_nominal{9,k} = 0 * ones(nx,1);
+    w_nominal{7,k} = ones(nu,1)*0.35;
+    w_nominal{8,k} = 0.71 * ones(nu,1);
+    w_nominal{9,k} = 0 * ones(nu,1);
     % initialise w
     
     w{1,k} = ones(nu,1)*0.1;
@@ -90,9 +90,9 @@ x_0 = [1 -1 -0.8 ;
        0  0    0 ;
        0  0    0];
 
+for m = 1:1  
+   
 %% Prediction
-
-
 
 for i = 1:M
     
@@ -126,47 +126,103 @@ for i = 1:M
     
 end
 
- 
+%% Communication 1
+
+% save in x global
+xc = cell(M,N+1);
+for i =1:M
+    for k = 1:N+1
+       
+        xc{i,k} = value(x{i,k});
+        
+    end  
+end
+
 %% Coordination
  
- 
+
  for i = 1:M
      
      constraints_2 = [];
      objective_2 = 0;
      
      for k = 1:N
+
+         J_1 = lambda((i-1)*(nu)+1:(i-1)*(nu)+nu)' * (xc{i,k}(1:nu) - w{i,k});
          
-         J_1 = lambda((i-1)*(nu)+1:(i-1)*(nu)+nu)'* (x{i,k}(1:nu) - w{i,k});
-         
-         J_2 = rho * 0.5 * ((x{i,k}(1:nu) - w{i,k})'*(x{i,k}(1:nu) - w{i,k}));
+         J_2 = rho * 0.5 * ((xc{i,k}(1:nu) - w{i,k})' * (xc{i,k}(1:nu) - w{i,k}));
          
          J_3 = 0;
          
-         for j = 1:N_j
-           if (i~=j)  
-             J_3 = J_3 + lambda_to_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j)'* (x{j,k}(1:nu) - w_to_j{((i-1)*M+j),k}) ...
-                 + rho * 0.5 * (x{j,k}(1:nu) - w_to_j{((i-1)*M+j),k})' * (x{j,k}(1:nu) - w_to_j{((i-1)*M+j),k}) ; 
+          for j = 1:M
+            if (i~=j)  
+             J_3 = J_3 + lambda_to_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j)'* (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k}) ...
+                 + rho * 0.5 * (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k})' * (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k}) ; 
  
-            eta_ij = (w_nominal{i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu)) * 1/norm(w_nominal{i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu));
-            part_g = (w{i,k}(1:nu) - w_to_j{((i-1)*M+j),k}(1:nu)) - (w_nominal{i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu)) - delta;
+             eta_ij = (w_nominal{(i-1)*M+i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu)) * 1/norm(w_nominal{(i-1)*M+i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu));
+             part_g = (w{i,k}(1:nu) - w_to_j{((i-1)*M+j),k}(1:nu)) - (w_nominal{i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu)) - delta;
             
-            constraints_2 = [constraints_2, eta_ij' * part_g >= 0];
+             constraints_2 = [constraints_2, eta_ij' * part_g >= 0];
             
-           end
-         end
+            end
+          end
                 
          objective_2 = objective_2 + J_1 + J_2 + J_3;
     
      end
+     
+     optimize([constraints_2],objective_2);
     
-     optimize(constraints_2,objective_2);
+ end
+
+
+%% Mediation
+
+for i = 1:M
+    
+    for k = 1:N
+    
+    lambda((i-1)*(nu)+1:(i-1)*(nu)+nu) = lambda((i-1)*(nu)+1:(i-1)*(nu)+nu) + rho * (x{i,k}(1:nu) - w{i,k});
+    
+        for j = 1:M
+            if (i~=j)
+            lambda_to_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j) = lambda_to_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j) ...
+                + rho * (x{j,k}(1:nu) - w_to_j{((i-1)*M+j),k});
+            end
+        end
+    
+    end
     
 end
 
 
+%% Communication 2
+
+for i = 1:M
+    
+    w_from_j_to_i = w_from_j((i-1)*M*nu+1:(i-1)*M*nu+nu*M,:);
+    
+    for j = 1:M
+        
+        if (i~= j)
+            
+            lambda_from_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j)= lambda_to_j((j-1)*(nu)+1:(j-1)*(nu)+nu,i);
+            
+            for k = 1:N
+                
+                w_from_j_to_i((j-1)*(nu)+1:(j-1)*(nu)+nu,k) = w_to_j{(j-1)*nu+i,k};
+                
+            end
+        end
+        
+    end
+    
+end
+
+m
+end
 %% Visualisation
 
 
-
+admm_visualise (r,x,N,T);
 
