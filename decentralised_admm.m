7bc4b045c31d0ffe6abf91de74f673d9b37bd064
Aren Karapetyan <karapetyanaren@gmail.com>
2020-01-20 17:40:46 +0000

    deletion of obsolete files

--- decentralised_admm.m
+++ decentralised_admm.m
@@ -1,5 +1,4 @@
-
-%% Initialisation
+  %% Initialisation
 
 yalmip('clear')
 clear all
@@ -17,132 +16,156 @@ nu = 2; % Number of inputs
 
 % MPC data
 N = 100;
-Q = eye(nu)*2;
-R = eye(nu)*1;
+Q = eye(nu)*14;
+R = eye(nu)*5;
 
 % Number of iterations
-it = 3 ;
+it = 15;
 
 M = 3; % Number of agents
 N_j = M-1; % Number of neighbours 
 delta = 0.2; % Inter-agent distance 
-rho = 1;
+rho = 15;
 
 
-% initialize the states, reference, control and nominal states for each
-% agent
+% position reference setpoint
+% 
+%   r =[0.5  0    0;
+%       1  0.5  0.5;];
+r(1,:) =[0.5,1];
+r(2,:) =[0,0.5];
+r(3,:) =[0,0.5];
+
+% w and w_from_j
+
+wc = cell(M,N);
+w_from_jc = cell(M,M,N);
+
+
+% lambda and lambda_from_j
+
+lambda = cell(M,N+1);
+lambda_from_j = cell(M,M,N);
+lambda_to_j = cell(M,M,N);
+
 for i = 1:M
     
     % position and acceleration
     x(i,:) = sdpvar(repmat(nx,1,N+1),repmat(1,1,N+1));
     a(i,:) = sdpvar(repmat(nu,1,N),repmat(1,1,N));
-    
-    % position reference setpoint
-    %r{i} = sdpvar(nu*N,1);
-    r{i} = ones(nu*N,1)*0;
-    
-    % nominal position 
-    %w_nominal(i,:) = sdpvar(repmat(nx,1,N+1),repmat(1,1,N+1));
-    
-
-    % w and w_to_j
     w(i,:) = sdpvar(repmat(nu,1,N+1),repmat(1,1,N+1));
-    for j = 1:(M)
-        w_to_j((i-1)*(M)+j,:) = sdpvar(repmat(nu,1,N+1),repmat(1,1,N+1));
+    
+    % w_from_jc initialisation
+    for j = 1:M
+        w_to_j(i,j,:) = sdpvar(repmat(nu,1,N+1),repmat(1,1,N+1));
+        for k =1:N
+            w_from_jc{i,j,k} = [0;0];
+            
+            % w_to_j
+            %w_to_jc{i,j,k} = [0.1;0.1];
+            
+            lambda_from_j{i,j,k} = [5;5];
+            lambda_to_j{i,j,k} = [5;5];
+        end
     end
+    
 end
-
-    % w_from_j
-    w_from_j = ones(M*(M)*nu,N+1);
-
-    % lambda, lambda_to_j and lambda_from_j
-    lambda = ones(M*nu,N+1) * 0.1;
-%   lambda_to_j = ones(M*nu,M) * 0.01;
-%   lambda_from_j = ones(M*nu,M) * 0.01;
-    lambda_to_j = ones(M*M*nu,N+1) * 0.1;
-    lambda_from_j = ones(M*M*nu,N+1) * 0.1;
-
-wc = cell(M,N);
 
 for k = 1:N
     
-    %  populate nominal values
-    w_nominal{1,k} = ones(nu,1)*0.45;
-    w_nominal{2,k} = 0 * ones(nu,1);
-    w_nominal{3,k} = -0.03 * ones(nu,1);
+    lambda{1,k} = [5;5];
+    lambda{2,k} = [5;5];
+    lambda{3,k} = [5;5];
     
-    w_nominal{4,k} = ones(nu,1)*0.45;
-    w_nominal{5,k} = 0 * ones(nu,1);
-    w_nominal{6,k} = -0.03 * ones(nu,1);
-    
-    w_nominal{7,k} = ones(nu,1)*0.45;
-    w_nominal{8,k} = 0 * ones(nu,1);
-    w_nominal{9,k} = -0.03 * ones(nu,1);
-    % initialise w
-    
-    wc{1,k} = ones(nu,1)*0;
-    wc{2,k} = 0.0 * ones(nu,1);
-    wc{3,k} = -0.0 * ones(nu,1);
-
 end
 
 % initial conditions
 
 
-x_0 = [1 -1 -1 ;
-       2  2 -2 ; 
-       0  0    0 ;
-       0  0    0];
-
-for m = 1:it
+x_0 = [0.5   1   1 ;
+       0   0.5 1.5 ; 
+       0     0   0 ;
+       0     0   0];
    
+ops = sdpsettings('verbose',0);
+
+% initialise w-s
+for i = 1:M
+   
+    objective =0;
+    constraints =[];
+    for k = 1:N
+        
+        objective = objective + (x{i,k}(1:nu)-r(i,:)')'* Q * ...
+            (x{i,k}(1:nu)-r(i,:)') + a{i,k}'* R * a{i,k};
+        
+        constraints = [constraints, x{i,k+1} == A*x{i,k} + B*a{i,k}, x{i,1} == x_0(:,i),x{i,N+1}(3:4) == [0;0],a{i,N} == [0;0]];       
+    end
+    
+    optimize(constraints,objective);
+    
+    for k =1:(N+1)
+       wc{i,k} = value(x{i,k}(1:nu));
+       ref{i,k} = value(x{i,k}(1:nu));
+       
+    end
+    
+end   
+
+for i = 1:M
+ for k = 1:N 
+   for j = 1:M
+      if i~=j
+         w_from_jc{i,j,k} = value(x{j,k}(1:nu));
+         w_to_jc{i,j,k} = value(x{j,k}(1:nu));
+      end
+   end 
+ end
+end
+
+xprev=ref;
+
+for m =1:it
+
+sum = 0;    
+    
 %% Prediction
-
-
-
 
 for i = 1:M
     
-    % ????
-    %x(i,:) = sdpvar(repmat(nx,1,N+1),repmat(1,1,N+1));
-    %a(i,:) = sdpvar(repmat(nu,1,N),repmat(1,1,N));
-    % ???
-    
-    w_from_j_to_i = w_from_j((i-1)*M*nu+1:(i-1)*M*nu+nu*M,:);
-    
+
     constraints_1 = [];
     objective_1 = 0;
     
      for k = 1:N
          
-         J = (x{i,k}(1:nu) - r{i}((k-1)*nu+1:(k-1)*nu+nu))'* Q * ...
-             (x{i,k}(1:nu) - r{i}((k-1)*nu+1:(k-1)*nu+nu)) + a{i,k}'* R * a{i,k};
+         J = (x{i,k}(1:nu) - ref{i,k})'* Q * ...
+             (x{i,k}(1:nu) - ref{i,k}) + a{i,k}'* R * a{i,k};
          
-         J_1 = lambda((i-1)*(nu)+1:(i-1)*(nu)+nu,k)'* (x{i,k}(1:nu) - wc{i,k});
-         
+         J_1 = lambda{i,k}'* (x{i,k}(1:nu) - wc{i,k});
+          
          J_2 = rho * 0.5 * ((x{i,k}(1:nu) - wc{i,k})'*(x{i,k}(1:nu) - wc{i,k}));
-        
+         
          J_3 = 0;
          for j = 1:M
             if (i ~= j)
-%             J_3 = J_3 + lambda_from_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j)'* (x{i,k}(1:nu) - w_from_j_to_i((j-1)*(nu)+1:(j-1)*(nu)+nu,k)) ...
-%                 + rho * 0.5 * (x{i,k}(1:nu) - w_from_j_to_i((j-1)*(nu)+1:(j-1)*(nu)+nu,k))' * (x{i,k}(1:nu) - w_from_j_to_i((j-1)*(nu)+1:(j-1)*(nu)+nu,k));
-              J_3 = J_3 + lambda_from_j((i-1)*M*nu+(j-1)*(nu)+1:(i-1)*M*nu+(j-1)*(nu)+nu,k)'* (x{i,k}(1:nu) - w_from_j_to_i((j-1)*(nu)+1:(j-1)*(nu)+nu,k)) ...
-                  + rho * 0.5 * (x{i,k}(1:nu) - w_from_j_to_i((j-1)*(nu)+1:(j-1)*(nu)+nu,k))' * (x{i,k}(1:nu) - w_from_j_to_i((j-1)*(nu)+1:(j-1)*(nu)+nu,k));
-
+             J_3 = J_3 + lambda_from_j{i,j,k}'* (x{i,k}(1:nu) - w_from_jc{i,j,k}) ...
+                 + rho * 0.5 * (x{i,k}(1:nu) - w_from_jc{i,j,k})' * (x{i,k}(1:nu) - w_from_jc{i,j,k});
             end 
          end
          
-         constraints_1 = [constraints_1, x{i,k+1} == A*x{i,k} + B*a{i,k}];
+         constraints_1 = [constraints_1, x{i,k+1} == A*x{i,k} + B*a{i,k}];%,x{i,k}(1)<=2,x{i,k}(1)>=-2,x{i,k}(2)<=2,x{i,k}(2)>=-2];
          objective_1 = objective_1 + J + J_1 + J_2 + J_3;
-     end
-     
-     optimize([constraints_1, x{i,1} == x_0(:,i)],objective_1);
+      end
+      
+      optimize([constraints_1, x{i,1} == x_0(:,i),x{i,N}(3:4) == [0;0],a{i,N} == [0;0]],objective_1,ops);
     
 end
 
-%% Communication 1
 
+
+
+%% Communication 1
 % save in x global
 xc = cell(M,N+1);
 for i =1:M
@@ -153,130 +176,104 @@ for i =1:M
     end  
 end
 
+
+if m>1
+for i =1:M
+    for k = 1:N+1
+       
+        wc{i,k} = value(w{i,k});
+        for j =1:M
+            if (i~=j)
+             w_to_jc{i,j,k} = value(w_to_j{i,j,k});   
+            end
+        end
+    end  
+end
+end
 %% Coordination
 
 
- for i = 1:M
-     
-     constraints_2 = [];
-     objective_2 = 0;
-     
-     % ?????
-      w(i,:) = sdpvar(repmat(nu,1,N+1),repmat(1,1,N+1));
-     % ????
-     
-     for k = 1:N
-
-         J_1 = lambda((i-1)*(nu)+1:(i-1)*(nu)+nu,k)' * (xc{i,k}(1:nu) - w{i,k});
-         
-         J_2 = rho * 0.5 * ((xc{i,k}(1:nu) - w{i,k})' * (xc{i,k}(1:nu) - w{i,k}));
-         
-         J_3 = 0;
-         
-          for j = 1:M
-            if (i~=j)  
-%              J_3 = J_3 + lambda_to_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j)'* (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k}) ...
-%                  + rho * 0.5 * (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k})' * (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k}) ; 
-             J_3 = J_3 + lambda_to_j((i-1)*M*nu+(j-1)*(nu)+1:(i-1)*M*nu+(j-1)*(nu)+nu,k)'* (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k}) ...
-                 + rho * 0.5 * (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k})' * (xc{j,k}(1:nu) - w_to_j{((i-1)*M+j),k}) ; 
-             eta_ij = (w_nominal{(i-1)*M+i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu)) * 1/norm(w_nominal{(i-1)*M+i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu));
-             part_g = (w{i,k}(1:nu) - w_to_j{((i-1)*M+j),k}(1:nu)) - (w_nominal{i,k}(1:nu) - w_nominal{((i-1)*M+j),k}(1:nu)) - delta;
-            
-             constraints_2 = [constraints_2, eta_ij' * part_g >= 0];
-            
-            end
+for i = 1:M
+    
+    constraints_2 = [];
+    objective_2 = 0;
+    
+    for k = 1:N
+      
+      J_1 =  lambda{i,k}' * (xc{i,k}(1:nu) - w{i,k}) + rho*0.5*(xc{i,k}(1:nu) - w{i,k})'*(xc{i,k}(1:nu) - w{i,k});
+      %objective_2 = objective_2+J_1;
+      
+      J_2 =0;
+      for j =1:M
+          if (i~=j)
+              J_2 = J_2 + lambda_to_j{i,j,k}'*(xc{j,k}(1:nu)-w_to_j{i,j,k})+rho*0.5*(xc{j,k}(1:nu)-w_to_j{i,j,k})'*(xc{j,k}(1:nu)-w_to_j{i,j,k});
+          
+%               eta_ij = (wc{i,k} - w_to_jc{i,j,k}) * 1/norm(wc{i,k} - w_to_jc{i,j,k});
+%               h_ij = eta_ij' * ((w{i,k} - w_to_j{i,j,k})-(wc{i,k} - w_to_jc{i,j,k})) - delta;
+% 
+              eta_ij = (xc{i,k}(1:nu) - xc{j,k}(1:nu)) * 1/norm(xc{i,k}(1:nu) - xc{j,k}(1:nu));
+              h_ij = eta_ij' * ((w{i,k} - w_to_j{i,j,k})-(xc{i,k}(1:nu) - xc{j,k}(1:nu))) - delta;
+%               
+%              constraints_2 = [constraints_2, norm(wc{i,k} - w_to_jc{i,j,k}) + h_ij >= 0];
+               constraints_2 = [constraints_2, norm(xc{i,k}(1:nu) - xc{j,k}(1:nu)) + h_ij >= 0];
           end
-                
-          objective_2 = objective_2 + J_1 + J_2 + J_3;
-    
+          
       end
-     
-      optimize(constraints_2,objective_2);
-    
- end
-
-
-% nominal update
-for i = 1:M 
- 
-    for k = 1:N
-   
-        w_nominal{(i-1)*M+i,k}(1:nu) = w{i,k};
-        
-        for j = 1:M
-            if (i~=j)
-                
-               w_nominal{((i-1)*M+j),k}(1:nu) = w_to_j{((i-1)*M+j),k}(1:nu);
-                
-            end
-        end
-    
+      objective_2 = objective_2 + J_1+J_2;
     end
-    
-end
-
-% make w constant
-
-for i =1:M
-    for k = 1:N
        
-        wc{i,k} = value(w{i,k});
-        
-    end  
+    optimize(constraints_2,objective_2,ops);
+ 
 end
 
 %% Mediation
 
-for i = 1:M
+for i =1:M
     
-    for k = 1:N
-    
-    lambda((i-1)*(nu)+1:(i-1)*(nu)+nu,k) = lambda((i-1)*(nu)+1:(i-1)*(nu)+nu,k) + rho * (x{i,k}(1:nu) - w{i,k});
-    
-         for j = 1:M
-             if (i~=j)
-%              lambda_to_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j) = lambda_to_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j) ...
-%                  + rho * (x{j,k}(1:nu) - w_to_j{((i-1)*M+j),k});
+    for k=1:N
+        lambda{i,k} = lambda{i,k} + rho * (xc{i,k}(1:nu)-value(w{i,k}));
 
-            lambda_to_j((i-1)*M*nu+(j-1)*(nu)+1:(i-1)*M*nu+(j-1)*(nu)+nu,k) = lambda_to_j((i-1)*M*nu+(j-1)*(nu)+1:(i-1)*M*nu+(j-1)*(nu)+nu,k) ...
-                 + rho * (x{j,k}(1:nu) - w_to_j{((i-1)*M+j),k});
-             end
-         end
-    
+    for j = 1:M
+        if i~=j
+        lambda_to_j{i,j,k} = lambda_to_j{i,j,k} + rho * (xc{j,k}(1:nu)-value(w_to_j{i,j,k}));
+        end
     end
-    
+    end
 end
-
 
 %% Communication 2
 
 for i = 1:M
-    
-   % w_from_j_to_i = w_from_j((i-1)*M*nu+1:(i-1)*M*nu+nu*M,:);
-    
-    for j = 1:M
-        
-        if (i~= j)
-            
-            %lambda_from_j((i-1)*(nu)+1:(i-1)*(nu)+nu,j)= lambda_to_j((j-1)*(nu)+1:(j-1)*(nu)+nu,i);
-            
-            for k = 1:N
-                
-              %  w_from_j_to_i((j-1)*(nu)+1:(j-1)*(nu)+nu,k) = w_to_j{(j-1)*nu+i,k};
-             lambda_from_j((i-1)*M*nu+(j-1)*(nu)+1:(i-1)*M*nu+(j-1)*(nu)+nu,k) = lambda_to_j((j-1)*M*nu+(i-1)*nu+1:(j-1)*M*nu+i*nu,k);
-              w_from_j((i-1)*M*nu+(j-1)*(nu)+1:(i-1)*M*nu+(j-1)*(nu)+nu,k) = w_to_j{(j-1)*M+i,k};
+    for k = 1:N
+        for j =1:M
+            if i~=j
+           lambda_from_j{i,j,k} = lambda_to_j{j,i,k};
+           w_from_jc{i,j,k} = value(w_to_j{j,i,k});
             end
         end
-        
     end
-    
 end
 
-m
+% error
+
+
+s1 = sprintf('iteration: %d \n', m );
+disp(s1);
+for k = 1:N
+   sum = sum + abs(value(norm(x{1,k}(1:nu) - xprev{1,k}(1:nu))));
 end
+
+s2 = sprintf('error: %d \n', sum);
+disp(s2);
+
+for k =1:N
+    xprev{1,k} = value(x{1,k}(1:nu));
+end
+
+end
+
+
 
 %% Visualisation
 
-
-admm_visualise (r,w,N,T);
-
+admm_visualise(r,x,N,T);
