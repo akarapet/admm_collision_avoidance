6521899e9a429ac4e15fb267113785e3a4fd99a2
Aren Karapetyan <karapetyanaren@gmail.com>
2020-01-29 23:12:49 +0000

    mpc initial not working

--- centralised_collision_avoidance_mpc.m
+++ centralised_collision_avoidance_mpc.m
@@ -80,24 +80,17 @@ end
    
     
 %% Definition
-for m = 1:3 
-    
 constraints = [];
 objective = 0;
 
-    for i =1:M
-      
-      constraints = [constraints, x{i,1} == x_0(:,i),x{i,N+1}(3:4) == [0;0],a{i,N} == [0;0]];
-        
-      for k = 1:N
-      x_nominal{i,k} = value(x{i,k}(1:nu));  
-      end
+    for i =1:M     
+      constraints = [constraints,x{i,N+1}(3:4) == [0;0],a{i,N} == [0;0]];
+         for k = 1:N
+            x_nominal{i,k} = value(x{i,k}(1:nu));  
+         end
     end 
 
 for k = 1:N
-   
-
-   
     for i = 1:M
         
         objective = objective  + (x{i,k}(1:nu) - ref{i,k})'* Q * ...
@@ -121,18 +114,41 @@ for k = 1:N
 
 end
 
+parameters_in = {x{1,1},x{2,1},x{3,1}};
+solutions_out = {[a{1,1}],[a{2,1}],[a{3,1}]};
     
     %optimize([constraints, x{1,1} == x_1_0,x{2,1} == x_2_0,x{3,1} == x_3_0,x{1,N+1}(3:4) ==[0;0],x{2,N+1}(3:4) == [0;0],x{3,N+1}(3:4) == [0;0],a{1,N} == [0;0],a{2,N} == [0;0],a{3,N} == [0;0]],objective);
-    optimize(constraints,objective);
-    m
-       
-end
+    %optimize(constraints,objective);
+    controller = optimizer(constraints, objective,[],parameters_in,solutions_out);
 
+x{1} = x_0(:,1);
+x{2} = x_0(:,2);
+x{3} = x_0(:,3);
+for m = 1:100
+    
+    inputs = {x{1},x{2},x{3}};
+    
+    [solutions,diagnostics] = controller{inputs}; 
+
+       
+    x{1} = A*x{1} + B*solutions{1};
+    x{2} = A*x{2} + B*solutions{2};
+    x{3} = A*x{3} + B*solutions{3};
+    for i =1:M
+         
+     implementedX{i,m} =  value(x{i,m}(1:nu));
+     for k = 1:N
+      x_nominal{i,k} = value(x{i,k}(1:nu));  
+      end
+    end 
+   
+  
+end
 
 
 %% Visualisation
 
-admm_visualise (r,x,N,T);
+admm_visualise (r,implementedX,N-1,T);
 %admm_visualise([0.5;1.3],x,N,T);
 
 
